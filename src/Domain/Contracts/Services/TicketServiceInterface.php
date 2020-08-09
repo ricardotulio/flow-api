23be1a037ce440cc56ebdd1f23726e243f4abb77
ricardotulio <ledo.tulio@gmail.com>
2020-08-09 14:22:17 +0100

    Ticket: create a contract for ticket services
    Create a contract that represents a service responsible for provide
    tickets.

---
+++ src/Domain/Contracts/Services/TicketServiceInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Flow\Domain\Contracts\Services;
+
+use Flow\Domain\Entities\Ticket\TicketInterface;
+use Flow\Domain\Entities\Ticket\TicketCollectionInterface;
+
+interface TicketServiceInterface
+{}
